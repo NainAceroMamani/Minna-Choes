7516a9e4a1ba9fe7b1e464de69cc34f606f4ed2f
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 00:18:09 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -1,4 +1,14 @@
 <?php
     if(!$_GET)  header('Location: http://64.225.53.166/?pagina=1');
-    
+    if(isset($_GET['pagina'])) :
+
+    else :
+        if(isset($_GET['prod'])) {
+            $datos_get = explode('-',$_GET['prod']);
+            $num= count($datos_get);
+        }
+        $pesos = new monedaModel();
+        $pesos->id = 1;
+        $peso = $pesos->one();
+    endif;
 	
