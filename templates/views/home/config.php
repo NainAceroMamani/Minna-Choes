1e3967ca15391fb12afdf5884cd5a4766ddda301
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 00:13:21 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -1,47 +1,4 @@
 <?php
     if(!$_GET)  header('Location: http://64.225.53.166/?pagina=1');
     
-        if(isset($_GET['pagina'])) :
-        $producto = new productoModel();
-        $data =  $producto->detail_productos();
-
-        $articulos_x_pagina = 9;
-        $total = sizeof($data);
-        $paginas = ceil($total/$articulos_x_pagina);
-
-        if(isset($_GET['action'])) :
-            if($_GET['action'] == 4) :
-                $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
-                $datos = $producto->detail_productos_categoria($inicio, $articulos_x_pagina,$_GET['id']);
-            else :
-                $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
-                $datos = $producto->detail_productos_paginate($inicio, $articulos_x_pagina);
-            endif;
-        else: 
-            $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
-            $datos = $producto->detail_productos_paginate($inicio, $articulos_x_pagina);
-        endif;
-        
-        $categoria = new categoriaModel();
-        $categorias =  $categoria->all();
-        $num = 0;
-        if(isset($_GET['prod'])) {
-            $datos_get = explode('-',$_GET['prod']);
-            $num= count($datos_get);
-        }
-        $pesos = new monedaModel();
-        $pesos->id = 1;
-        $peso = $pesos->one();
-
-        $producto_detal = new ProductoModel();
-        $productos_detal = $producto_detal->banner_producto();
-    else :
-        if(isset($_GET['prod'])) {
-            $datos_get = explode('-',$_GET['prod']);
-            $num= count($datos_get);
-        }
-        $pesos = new monedaModel();
-        $pesos->id = 1;
-        $peso = $pesos->one();
-    endif;
 	
