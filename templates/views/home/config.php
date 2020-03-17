ebd58736179352a2d1b3120b0bbcff164c1dddf2
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 00:18:51 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -1,6 +1,11 @@
 <?php
     if(!$_GET)  header('Location: http://64.225.53.166/?pagina=1');
     if(isset($_GET['pagina'])) :
+	$producto = new productoModel();
+        $data =  $producto->detail_productos();
+        $articulos_x_pagina = 9;
+        $total = sizeof($data);
+        $paginas = ceil($total/$articulos_x_pagina);
 
     else :
         if(isset($_GET['prod'])) {
