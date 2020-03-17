cba355715e2d392c332ff13ce26fe38d913d313d
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 01:10:17 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -6,6 +6,18 @@
 	$articulos_x_pagina = 9;
         $total = sizeof($data);
         $paginas = ceil($total/$articulos_x_pagina);
+	if(isset($_GET['action'])) :
+            if($_GET['action'] == 4) :
+                $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
+                $datos = $producto->detail_productos_categoria($inicio, $articulos_x_pagina,$_GET['id']);
+            else :
+                $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
+                $datos = $producto->detail_productos_paginate($inicio, $articulos_x_pagina);
+            endif;
+        else: 
+            $inicio = ($_GET['pagina'] -1)*$articulos_x_pagina;
+            $datos = $producto->detail_productos_paginate($inicio, $articulos_x_pagina);
+        endif;
     else :
         if(isset($_GET['prod'])) {
             $datos_get = explode('-',$_GET['prod']);
