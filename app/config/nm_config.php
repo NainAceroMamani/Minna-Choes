167affff0ec577710aa1eeebfdff18a7476fb549
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-16 23:38:22 -0500

    Update nm_config.php

--- app/config/nm_config.php
+++ app/config/nm_config.php
@@ -6,13 +6,13 @@
 
     define('LANG','es');                                                            /* Language */
 
-    define('BASEPATH', IS_LOCAL ? '/Minnsa-Shoes/' : '__EL BASEPATH EN PRODUCCIÓN__');
+    define('BASEPATH', IS_LOCAL ? '/Minnsa-Shoes/' : '64.225.53.166/');
 
     define('AUTH_SALT', 'NuamiFramework<3');                                        /* Sal del Sistema */
 
     define('PORT','80');
 
-    define('URL', IS_LOCAL ? 'http://127.0.0.1:'.PORT.'/Minnsa-Shoes/' : '__URL EN PRODUCCIÓN_');
+    define('URL', IS_LOCAL ? 'http://127.0.0.1:'.PORT.'/Minnsa-Shoes/' : '64.225.53.166/');
 
     // rutas de directorios y archivos =>
 
@@ -50,11 +50,11 @@
 
     // credenciales base de datos   =>  producción
     define('DB_ENGINE' ,   'mysql');
-    define('DB_HOST'   ,   'localhost');
-    define('DB_NAME'   ,   '__REMOTE DB__');
-    define('DB_USER'   ,   '__REMOTE DB__');
-    define('DB_PASS'   ,   '__REMOTE DB__');
-    define('DB_CHARSET',   '__REMOTE CHARTSET__');
+    define('DB_HOST'   ,   '127.0.0.1');
+    define('DB_NAME'   ,   'zapateria');
+    define('DB_USER'   ,   'root');
+    define('DB_PASS'   ,   'lobitogeiner24');
+    define('DB_CHARSET',   'utf8');
 
     /**
      * Controlador Defecto
