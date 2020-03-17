f4b8ad8b10aca6f888402d52fbe1a9fed214915f
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 01:00:05 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -1,7 +1,7 @@
 <?php
     if(!$_GET)  header('Location: http://64.225.53.166/?pagina=1');
     if(isset($_GET['pagina'])) :
-        $producto = new productoController();
+        $producto = new homeController();
     else :
         if(isset($_GET['prod'])) {
             $datos_get = explode('-',$_GET['prod']);
