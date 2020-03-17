081b5680872858ce8fcb3ecca82e42700d8f9c18
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2020-03-17 01:09:41 -0500

    Update config.php

--- templates/views/home/config.php
+++ templates/views/home/config.php
@@ -3,6 +3,9 @@
     if(isset($_GET['pagina'])) :
         $producto = new productoModel();
         $data =  $producto->detail_productos();
+	$articulos_x_pagina = 9;
+        $total = sizeof($data);
+        $paginas = ceil($total/$articulos_x_pagina);
     else :
         if(isset($_GET['prod'])) {
             $datos_get = explode('-',$_GET['prod']);
